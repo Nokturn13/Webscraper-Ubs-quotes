9c186dac4af52525f77f64811337651239ade67e
Nokturn13 <87426401+Nokturn13@users.noreply.github.com>
2022-03-19 09:29:28 +0100

    Add files via upload

--- Webscraper/main.py
+++ Webscraper/main.py
@@ -1,13 +1,15 @@
-from multiprocessing.connection import wait
 import requests
 from bs4 import BeautifulSoup
 from openpyxl import load_workbook
 import time
 import datetime
+#In case the programm stops running, you should change the "Lex" value to the Number of the last box.
+#Ex. the programm wrote untill the box A20 in excel, change "Lex" to 20 and rerun the program. 
 lex = 1
 stop = False
 
 def main(nex):
+    #Imports data from Google Finance
     req = requests.get("https://www.google.com/finance/quote/UBSG:SWX")
     soup = BeautifulSoup(req.content, "html.parser" )
 
@@ -16,12 +18,13 @@ def main(nex):
     c_output = res.get_text()
     output = c_output.replace("CHF", "")
 
+    #Exports the data to the Excel spreadsheet
     wb = load_workbook('quotes.xlsx')
     ws = wb.active
     ws['A'+nex].value = output
     wb.save('quotes.xlsx')
 
-    
+#Programm loop  
 while stop == False :
     rn = str(datetime.datetime.now().time())
     
